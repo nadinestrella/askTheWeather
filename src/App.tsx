1278c91c340a5a1ad3dfa9b4fe8d2871536b6159
nadinestrella <nadinestrella@gmail.com>
2025-12-29 18:31:00 +0100

    added condition weatherData display

--- src/App.tsx
+++ src/App.tsx
@@ -122,12 +122,15 @@ function App() {
         )}
 
         {/* Welcome State */}
-        <div className="mt-16 text-center">
-          <Cloud className="w-24 h-24 mx-auto text-purple-300 mb-6" />
-          <p className="text-purple-400 text-lg">
-            Enter a city name to get started
-          </p>
-        </div>
+
+        {!weatherData && !loading && !error && (
+          <div className="mt-16 text-center">
+            <Cloud className="w-24 h-24 mx-auto text-purple-300 mb-6" />
+            <p className="text-purple-400 text-lg">
+              Enter a city name to get started
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
